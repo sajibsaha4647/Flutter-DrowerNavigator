b91af1def780e3292271ad9c8db25708d6743f03
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-07-24 17:50:47 +0600

    updated

---
+++ android/app/src/main/kotlin/com/example/drowernavigator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drowernavigator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.blue,
+    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: HomPage(),
+    );
+  }
+}
+
+class HomPage extends StatefulWidget {
+  const HomPage({Key? key}) : super(key: key);
+
+  @override
+  State<HomPage> createState() => _HomPageState();
+}
+
+class _HomPageState extends State<HomPage> {
+
+  final globalKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      key: globalKey,
+         appBar: AppBar(
+           title: Text("drower navigation"),
+           centerTitle: true,
+           backgroundColor: Colors.blue,
+         ),
+        drawer: Drawer(
+          child: Container(
+            alignment: Alignment.topLeft,
+            color: Colors.blue,
+            child: Column(
+              children: [
+                  Text("Home")
+              ],
+            ),
+          ),
+        ),
+      body: Container(
+        child: RaisedButton(onPressed: () {
+          globalKey.currentState!.openDrawer();
+        },
+
+        ),
+      ),
+    ));
+  }
+}
+
+
